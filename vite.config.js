fac4433bd8ef7bc7f365b7a722efa31bed598dee
Runnas <84087888+therunnas@users.noreply.github.com>
2025-08-13 15:41:54 -0300

    build: set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE para GitHub Pages (subpasta do repositório)
-  base: '/burger-shop-ui-/',
+  base: '/burger-shop-ui-/', // <- obrigatório para Pages
 })
